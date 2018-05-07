043d94b278ec5c24b198613639c2e76b71526a80
xunanhang@xiaoniu66@com <xn060263@xiaoniu66.com>
2018-05-07 11:08:34 +0800

    基于dedecms管理后台

---
+++ readme.txt
@@ -0,0 +1,6 @@
+准备工作：
+1、安装PHP运行环境   建议使用wampserver一键集成安装    自行百度安装方法
+2、下载dedecms内容管理系统、选择UTF-8版本   自行百度官网下载
+3、将解压好的dedecms文件放到wamp目录下的www目录下并在浏览器地址栏输入localhost运行并安装dedecms文件    具体步骤自行百度安装方法
+4、将本文件夹内容全部复制并替换到wamp目录下的www目录下
+5、分别在浏览器地址栏输入localhost和localhost/admin和localhost/phpmyadmin即可访问网站首页、网站管理后台和网站数据库
---
+++ templets/default/js/comm.js
@@ -0,0 +1,20 @@
+$(function() {
+	$("#J_headerCategory").mouseover(function() {
+		$(this).find(".nav-left-tab").stop().slideDown();
+	});
+	$("#J_headerCategory").mouseout(function() {
+		$(this).find(".nav-left-tab").stop().slideUp();
+	});
+
+	$(".clearfix li a.link").click(function() {
+		$(this).addClass("on").siblings("a").removeClass("on");
+	});
+
+});
+
+$(document).on("click", "#J_brandbtn a", function(){
+	var _val = $(this).attr("data-val");
+	$(this).addClass("selected").parent().siblings("li").find("a").removeClass("selected");
+	0 != _val ? ($("#J_brandlist li").hide(), $('#J_brandlist a[data-alpha="' + _val + '"]').parent().show()) : $("#J_brandlist li").show()
+});
+
